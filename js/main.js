18f7a3799d9b44f423881c4d39ccc8b2eac97d52
unknown <eliezermasiala2000@gmail.com>
2023-07-18 13:38:25 +0200

    finish page d'accueil

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let media768 = window.matchMedia("(max-width:768px)");
+console.log(media768)
+
+
+
+
+
+
+/* const navbar = document.getElementById('navbar')
+console.log(navbar)
+
+window.addEventListener('scroll',(e)=>{
+
+    e.preventDefault()
+    if(window.scrollY > 580){
+        navbar.innerHTML
+        navbar.style.position = 'fixed'
+    } else if(window.scrollY < 578){
+        navbar.innerHTML
+        navbar.style.position = 'relative'
+    }
+    
+}) */
